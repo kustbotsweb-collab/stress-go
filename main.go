545eb425172a4422b97cf1e9a52b882b1e4023b6
frozenbots60-source <frozenbots60@gmail.com>
2026-02-22 13:03:22 +0530

    Update main.go

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 // CONFIGURATION (STAY STEALTHY)
 // ==========================================
 var (
-	SERVER_URL      = getEnv("TARGET_URL", "https://www.ddwallet.vip/")
+	SERVER_URL      = getEnv("TARGET_URL", "https://khazaana.co.in/")
 	TOTAL_CLIENTS   = 3000           // Number of concurrent refresh clients
 	MAX_WORKERS     = 3000
 	REFRESH_DELAY   = 80 * time.Millisecond // Lower = heavier stress (80ms ≈ 375 RPS total)
