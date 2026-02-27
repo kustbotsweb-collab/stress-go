3e040063a6684c28a3380a6c92003a339db8de81
frozenbots60-source <frozenbots60@gmail.com>
2026-02-27 15:00:27 +0530

    Update main.go

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ import (
 // ==========================================
 var (
 	SERVER_URL      = getEnv("TARGET_URL", "https://teenpattibliss.co.in/")
-	TOTAL_CLIENTS   = 3000          // Number of concurrent refresh clients
-	MAX_WORKERS     = 3000
+	TOTAL_CLIENTS   = 300          // Number of concurrent refresh clients
+	MAX_WORKERS     = 300
 	REFRESH_DELAY   = 80 * time.Millisecond // Lower = heavier stress (80ms ≈ 375 RPS total)
 )
 
